47264a1528c06d4832cba41d931222f04830cc14
saherelgendy <saherelgendy1990@gmail.com>
2023-11-29 01:05:55 +0200

    creating and styling services section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,41 @@ import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import {
+  ArrowDownToLine,
+  CheckCircle,
+  ArrowUp01,
+  Leaf,
+  HeartHandshake,
+} from "lucide-react";
 
 export default function Home() {
+  const services = [
+    {
+      name: "Instant Delivery",
+      Icon: ArrowDownToLine,
+      description:
+        "get your assets delievered to your email in seconds and download them right a way",
+    },
+    {
+      name: "Guaranteed Quality",
+      Icon: CheckCircle,
+      description:
+        "Every asset quality in our collection is verified and ensured by our team against highest quality standards.",
+    },
+    {
+      name: "Instant Delivery",
+      Icon: ArrowUp01,
+      description:
+        "get your assets delievered to your email in seconds and download them right a way.",
+    },
+    {
+      name: "For the people",
+      Icon: HeartHandshake,
+      description: "We pledge 1% of our income to the charities ",
+    },
+  ];
+
   return (
     <>
       <MaxWidthWrapper>
@@ -13,7 +46,7 @@ export default function Home() {
             <span className="text-blue-600">digital assets</span>
           </h1>
 
-          <p className="mt-8 text-lg max-w-prose">
+          <p className="mt-8 text-lg max-w-prose text-muted-foreground">
             Welcome to ShopVista, quality of our products is ensured and
             guaranteed, you can shop anything anytime, we're always in the
             service
@@ -29,8 +62,28 @@ export default function Home() {
           {/* TODO: List Products */}
         </div>
       </MaxWidthWrapper>
-      <section>
+      <section className="border-t border-gray-200 bg-gray">
+        <MaxWidthWrapper className="py-20">
+          <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0">
+            {services.map((service) => (
+              <div
+                key={service.name}
+                className="text-center md:flex md:items-start md:text-left lg:block"
+              >
+                <div className="md:flex-shrink-0 flex justify-center">
+                  <div className="h-16 w-16 flex justify-center items-center rounded-full bg-blue-100 text-blue-900">
+                    <service.Icon className="w-1/3 h-1/3" />
+                  </div>
+                </div>
 
+                <div className="mt-6 text-center">
+                  <h3 className="text-base font-medium text-gray-900">{service.name}</h3>
+                  <p className="mt-3 text-sm text-muted-foreground">{service.description}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </MaxWidthWrapper>
       </section>
     </>
   );
