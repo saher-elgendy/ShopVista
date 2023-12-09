d1bb609dbf623308424ffe09b9164142e9fad2ce
saherelgendy <saherelgendy1990@gmail.com>
2023-12-09 16:27:08 +0200

    adjust navbar responsiveness

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -14,21 +14,21 @@ const Navbar = () => {
     <div className="sticky z-50 top-0 inset-x-0 h-16 w-full bg-white">
       <MaxWidthWrapper>
         <div className="border-b border-gray-200">
-          <div className="flex h-16 items-center">
+          <div className="flex h-16  justify-between items-center">
             <div className="ml-4 flex lg:ml-0">
               <Link href="/">
                 <Icons.logo className="h-10 w-10" />
               </Link>
             </div>
 
-            <div className="relative z-50 lg:ml-8 lg:self-stretch w-full">
+            <div className="relative hidden lg:block z-50 lg:ml-8 lg:self-stretch w-full">
               <NavItems />
             </div>
             <div className="flex justify-center items-center">
               {
                 !user ? (
                   <Link
-                    href="sign-in"
+                    href="./sign-in"
                     className={buttonVariants({
                       variant: 'ghost'
                     })}>
