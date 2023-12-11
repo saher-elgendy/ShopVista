e18ad85601eea2c1d43339911038aa9ae7d9cf73
saherelgendy <saherelgendy1990@gmail.com>
2023-12-11 20:12:47 +0200

    creating payload CMS client and caching it

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,23 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export const formatePrice = (
+  price: number | string,
+  options: {
+    currency?: 'USD' | 'EURO'| 'GBP' | 'BDT'
+    notation?: Intl.NumberFormatOptions['notation']
+  } = {}
+) => {
+  const {currency = "USD", notation= 'compact'} = options;
+
+  const numericPrice = typeof price === 'string' ? parseFloat(price) : price;
+
+  return new Intl.NumberFormat('en-us', {
+    style: 'currency',
+    currency,
+    notation,
+    maximumFractionDigits: 2,
+  }).format(numericPrice)
+}
---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+
+const app = express();
+
+const port = Number(process.env.port) || 3000;
+
+
+const start = () => {
+    
+}
+
+start();
+
