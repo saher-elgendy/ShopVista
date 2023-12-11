1d9e3cdd1550bfa46024b115f7dd5fd7a7b63012
saherelgendy <saherelgendy1990@gmail.com>
2023-12-11 21:01:39 +0200

    integrating payload cms with express

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { getPayloadClient } from './get-payload';
 
 
 const app = express();
@@ -6,7 +7,15 @@ const app = express();
 const port = Number(process.env.port) || 3000;
 
 
+
 const start = () => {
+    const payload = getPayloadClient({
+        initOptions: {
+            express: app,
+            onInit: async (cms) =>{
+                cms.logger.info(`Admin URL: ${cms.getAdminURL}`)
+            }
+        })
     
 }
 
