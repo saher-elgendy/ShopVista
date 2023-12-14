85fff9f33b8d0afe45ef30e36db0d3767e54a14d
saherelgendy <saherelgendy1990@gmail.com>
2023-12-14 23:34:48 +0200

    creating payload config file

---
+++ src/payload.config.ts
@@ -0,0 +1,38 @@
+import { webpackBundler } from '@payloadcms/bundler-webpack';
+import { mongooseAdapter } from '@payloadcms/db-mongodb';
+import { slateEditor } from '@payloadcms/richtext-slate';
+import dotenv from 'dotenv';
+import path from 'path';
+import { buildConfig } from 'payload/config';
+import { Payload } from 'payload/dist/payload';
+
+
+dotenv.config({
+	path: path.resolve(__dirname, '../.env')
+});
+
+export default buildConfig({
+	serverURL: process.env.NEXT_PUBLICK_SERVER_URL || '',
+	collections: [],
+	routes: {
+		admin: '/sell'
+	},
+	admin: {
+		bundler: webpackBundler(),
+		meta: {
+			titleSuffix: '- Shopvista',
+			favicon: '/favicon.ico',
+			ogImage: '/thumbnail.jpg'
+		}
+	},
+	rateLimit: {
+		max: 2000,
+	},
+	db: mongooseAdapter({
+		url: process.env.MONGODB_URL!,
+	}),
+	editor: slateEditor({}),
+    typescript: {
+		outputFile: path.resolve(__dirname, 'payload-types.ts')
+	}
+})
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,26 @@
 import express from 'express';
 import { getPayloadClient } from './get-payload';
-
+import { nextApp, nextHandler } from './next-utils';
 
 const app = express();
 
 const port = Number(process.env.port) || 3000;
 
-
-
 const start = () => {
-    const payload = getPayloadClient({
-        initOptions: {
-            express: app,
-            onInit: async (cms) =>{
-                cms.logger.info(`Admin URL: ${cms.getAdminURL}`)
-            }
-        })
-    
-}
+	const payload = getPayloadClient({
+		initOptions: {
+			express: app,
+			onInit: async (cms) => {
+				cms.logger.info(`Admin URL: ${cms.getAdminURL}`);
+			}
+		}
+	});
+
+	app.use((req, res) => nextHandler(req, res));
+	
+	nextApp.prepare().then()
+
+	
+};
 
 start();
-
