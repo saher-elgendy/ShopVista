0047861dca588617d76ef55cd20826efbacbacbc
saherelgendy <saherelgendy1990@gmail.com>
2023-12-16 05:01:30 +0200

    setting up routes

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,75 @@
-import express from 'express';
-import { getPayloadClient } from './get-payload';
-import { nextApp, nextHandler } from './next-utils';
+import express from 'express'
+import { getPayloadClient } from './get-payload'
+import { nextApp, nextHandler } from './next-utils'
+import { IncomingMessage } from 'http'
+import nextBuild from 'next/dist/build'
+import path from 'path'
+import { PayloadRequest } from 'payload/types'
+import { parse } from 'url'
 
-const app = express();
+const app = express()
+const PORT = Number(process.env.PORT) || 3000
 
-const port = Number(process.env.port) || 3000;
 
-const start = () => {
-	const payload = getPayloadClient({
-		initOptions: {
-			express: app,
-			onInit: async (cms) => {
-				cms.logger.info(`Admin URL: ${cms.getAdminURL}`);
-			}
-		}
-	});
+export type WebhookRequest = IncomingMessage & {
+  rawBody: Buffer
+}
 
-	app.use((req, res) => nextHandler(req, res));
-	
-	nextApp.prepare().then()
-};
+const start = async () => {
 
-start();
+  const payload = await getPayloadClient({
+    initOptions: {
+      express: app,
+      onInit: async (cms) => {
+        cms.logger.info(`Admin URL: ${cms.getAdminURL()}`)
+      },
+    },
+  })
+
+  if (process.env.NEXT_BUILD) {
+    app.listen(PORT, async () => {
+      payload.logger.info(
+        'Next.js is building for production'
+      )
+
+		//@ts-expect-error
+      await nextBuild(path.join(__dirname, '../'))
+
+      process.exit()
+    })
+
+    return
+  }
+
+  const cartRouter = express.Router()
+
+  cartRouter.use(payload.authenticate)
+
+  cartRouter.get('/', (req, res) => {
+    const request = req as PayloadRequest
+
+    if (!request.user)
+      return res.redirect('/sign-in?origin=cart')
+
+    const parsedUrl = parse(req.url, true)
+    const { query } = parsedUrl
+
+    return nextApp.render(req, res, '/cart', query)
+  })
+
+  app.use('/cart', cartRouter)
+
+  app.use((req, res) => nextHandler(req, res))
+
+  nextApp.prepare().then(() => {
+    payload.logger.info('Next.js started')
+
+    app.listen(PORT, async () => {
+      payload.logger.info(
+        `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
+      )
+    })
+  })
+}
+
+start()
