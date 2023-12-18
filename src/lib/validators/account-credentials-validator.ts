1f7b81dc30e2e317cf94c8fa706be2aa12fd3927
saherelgendy <saherelgendy1990@gmail.com>
2023-12-18 21:29:20 +0200

    creating Auth credentials validator

---
+++ src/lib/validators/account-credentials-validator.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+export const AuthCredentialator = z.object({
+    email: z.string().email(),
+    password: z.string().min(8, {
+        message: 'password must be at least 8 characters long.',
+    })
+})
+
+export type TAuthCredentaialsValidator = z.infer<
+ typeof AuthCredentialator
+>
